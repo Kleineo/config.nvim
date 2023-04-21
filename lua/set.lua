bafbb0c95c28ef5a303f82d3ef91e78678568102
Kleine <renatasgal420@gmail.com>
2023-04-21 15:02:00 +0300

    configure more vim options

--- lua/set.lua
+++ lua/set.lua
@@ -1,34 +1,43 @@
-vim.o.hlsearch = false
+vim.opt.guicursor = ''
 
-vim.wo.number = true
+vim.o.rnu = true
+vim.o.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.o.smartindent = true
+vim.o.autoindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append('@-@')
+
+vim.o.updatetime = 50
+-- vim.o.updatetime = 250
+-- vim.o.timeout = true
+-- vim.o.timeoutlen = 300
+
+vim.opt.colorcolumn = '80'
 
 vim.o.mouse = 'a'
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
+-- vim.o.clipboard = 'unnamedplus'
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeout = true
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+vim.o.completeopt = 'menuone,noselect,preview'
 
