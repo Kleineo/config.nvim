2adf58e24f2dbb3cc3884ad0cbfd776a1b5f7870
Kleine <renatasgal420@gmail.com>
2023-04-22 08:30:51 +0300

    make nvim transparent

--- init.lua
+++ init.lua
@@ -12,5 +12,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+vim.cmd('highlight SignColumn ctermbg=none guibg=none')
+vim.cmd('highlight LineNr ctermbg=none guibg=none')
+vim.cmd('highlight CursorLineNr ctermbg=none guibg=none')
+vim.cmd('highlight GitSignsAdd ctermbg=none guibg=none')
+
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
