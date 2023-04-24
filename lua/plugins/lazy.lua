95faf0bec64bfc2a98c68237b25c543c17ad7137
Kleine <renatasgal420@gmail.com>
2023-04-24 20:05:18 +0300

    formatting

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -2,26 +2,18 @@ return {
   'tpope/vim-fugitive',
   'tpope/vim-sleuth',
   'lewis6991/gitsigns.nvim',
+  'theprimeagen/vim-be-good',
+  'mbbill/undotree',
 
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup {}
-    end
-  },
-
+  { 'numToStr/Comment.nvim', opts = {} },
+  { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end },
   {
     'folke/which-key.nvim',
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
       require("which-key").setup({
-        window = { position = 'top' }
-      })
+        window = { position = 'top' } })
     end,
   },
-
-  { 'numToStr/Comment.nvim', opts = {} },
-  'theprimeagen/vim-be-good',
-  'mbbill/undotree',
 }
