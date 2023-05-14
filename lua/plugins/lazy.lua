2f9a74ecd8c4a533d2829256cc9ab4f58dfe72c1
Kleine <renatasgal420@gmail.com>
2023-05-14 20:57:32 +0300

    add colorizer and tailwind class sorter

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,12 +1,25 @@
 return {
   'tpope/vim-fugitive',
   'tpope/vim-sleuth',
-  'lewis6991/gitsigns.nvim',
   'theprimeagen/vim-be-good',
   'mbbill/undotree',
 
-  { 'numToStr/Comment.nvim', opts = {} },
-  { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end },
+  { 'numToStr/Comment.nvim',       opts = {} },
+  { 'norcalli/nvim-colorizer.lua', config = function() require 'colorizer'.setup() end },
+  { "windwp/nvim-autopairs",       config = function() require("nvim-autopairs").setup {} end },
+
+  {
+    'laytan/tailwind-sorter.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+    build = 'cd formatter && npm i && npm run build',
+    config = function()
+      require('tailwind-sorter').setup {
+        on_save_enabled = true,
+        on_save_pattern = { '*.html', '*.js', '*.jsx', '*.ts', '*.tsx', '*.vue'},
+      }
+    end
+  },
+
   {
     'folke/which-key.nvim',
     config = function()
