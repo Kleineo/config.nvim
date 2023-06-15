6373faad1bae3ebbd12ce7cf98a7aac54a3d48e2
Kleine <renatasgal420@gmail.com>
2023-06-15 14:50:04 +0300

    add nvim-tree remaps

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,8 +1,27 @@
 return {
   'nvim-tree/nvim-tree.lua',
   config = function()
-    require('nvim-tree').setup {}
+    -- vim.g.loaded_netrw = 1
+    -- vim.g.loaded_netrwPlugin = 1
+    require('nvim-tree').setup {
+      hijack_netrw = false,
+      view = { width = 50 },
+      renderer = {
+        add_trailing = true,
+        full_name = true,
+      },
+    }
 
-    vim.keymap.set('n', '<leader>b', require('nvim-tree.api').tree.toggle)
+    local tree = require('nvim-tree.api').tree
+
+    vim.keymap.set('n', '<leader>n', function()
+      tree.find_file { open = true, focus = true }
+    end, { desc = 'Find file in NvimTree' })
+
+    vim.keymap.set('n', '<leader>bt', tree.toggle)
+    vim.keymap.set('n', '<leader>bc', tree.collapse_all)
+
+    -- close all directories unrelated to current file
+    vim.keymap.set('n', '<leader>ba', function() tree.collapse_all(true) end)
   end
 }
