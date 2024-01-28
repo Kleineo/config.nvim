5b8bfab0b47b078c98fee0b2a1eb83fef271e5c8
Kleine <renatasgal420@gmail.com>
2024-01-28 22:14:37 +0200

    setup lsp servers via mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,7 @@ return {
     'folke/neodev.nvim',
     'williamboman/mason-lspconfig.nvim',
     'williamboman/mason.nvim',
-    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+    { 'j-hui/fidget.nvim', opts = {} },
   },
   config = function()
     local on_attach = function(client, bufnr)
@@ -16,7 +16,7 @@ return {
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
       end
 
-      local telescope = require('telescope.builtin')
+      local telescope = require 'telescope.builtin'
 
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
@@ -39,64 +39,81 @@ return {
       vim.keymap.set({ 'n', 'i' }, '<c-k>', vim.lsp.buf.signature_help, { desc = 'Signature Documentation' })
 
       if client.server_capabilities.colorProvider then
-        require("document-color").buf_attach(bufnr)
+        require 'document-color'.buf_attach(bufnr)
       end
     end
 
-    require('neodev').setup()
+    require 'neodev'.setup()
+    require 'mason'.setup()
+    require 'mason-lspconfig'.setup()
+    local lsp = require 'lspconfig'
+    local mason_lspconfig = require 'mason-lspconfig'
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-    local lsp = require 'lspconfig'
-    require("mason").setup()
-    require("mason-lspconfig").setup()
+    capabilities = require 'cmp_nvim_lsp'.default_capabilities(capabilities)
 
     local servers = {
-      cssls = {},
-      gopls = {},
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-        },
-      },
-      intelephense = {},
-      rust_analyzer = {},
-      -- sqlls = {},
-      tailwindcss = {},
-      yamlls = {},
+      'cssls',
+      'gopls',
+      'lua_ls',
+      'intelephense',
+      'rust_analyzer',
+      'tailwindcss',
+      'yamlls',
+      -- 'sqlls',
+      -- 'tsserver',
+      -- 'volar',
     }
 
-    for client in pairs(servers) do
-      lsp[client].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = servers[client],
-      }
-    end
+    mason_lspconfig.setup {
+      ensure_installed = servers,
+    }
 
-    lsp.volar.setup {
-      filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
-      capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        for _, server in ipairs(vim.lsp.get_active_clients({ name = 'tsserver' })) do
-          vim.lsp.get_client_by_id(server.id).stop()
-        end
-        on_attach(_, bufnr)
+    -- for client in pairs(servers) do
+    --   lsp[client].setup {
+    --     on_attach = on_attach,
+    --     capabilities = capabilities,
+    --     settings = servers[client],
+    --   }
+    -- end
+
+    mason_lspconfig.setup_handlers {
+      function(server_name)
+        lsp[server_name].setup {
+          capabilities = capabilities,
+          on_attach = on_attach,
+          settings = servers[server_name],
+          -- filetypes = (servers[server_name] or {}).filetypes,
+        }
       end,
-    }
 
-    lsp.tsserver.setup {
-      capabilities = capabilities,
-      on_attach = function(client, bufnr)
-        local volarEnabled = #vim.lsp.get_active_clients({ name = 'volar' }) > 0
+      ['volar'] = function()
+        lsp.volar.setup {
+          filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+          capabilities = capabilities,
+          on_attach = function(_, bufnr)
+            -- stop tsserver if enabled
+            for _, server in ipairs(vim.lsp.get_active_clients { name = 'tsserver' }) do
+              vim.lsp.get_client_by_id(server.id).stop()
+            end
+            on_attach(_, bufnr)
+          end,
+        }
+      end,
 
-        if volarEnabled then -- stop tsserver
-          vim.lsp.get_client_by_id(client.id).stop()
-          return
-        end
-        on_attach(_, bufnr)
+      ['tsserver'] = function()
+        lsp.tsserver.setup {
+          capabilities = capabilities,
+          on_attach = function(client, bufnr)
+            local volarEnabled = #vim.lsp.get_active_clients { name = 'volar' } > 0
+
+            if volarEnabled then -- stop tsserver
+              vim.lsp.get_client_by_id(client.id).stop()
+              return
+            end
+            on_attach(_, bufnr)
+          end,
+        }
       end,
     }
   end,
