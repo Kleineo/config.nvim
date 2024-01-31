f8e58482b304035bdfacb8c7384daf132aa5e7d7
Kleine <renatasgal420@gmail.com>
2024-01-31 17:26:08 +0200

    add current buffer lsp server names to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,38 @@
+local function lsp_status()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.get_active_clients { bufnr = bufnr }
+  if next(clients) == nil then
+    return ''
+  end
+
+  local lsp_names = {}
+  for _, client in pairs(clients) do
+    table.insert(lsp_names, client.name)
+  end
+
+  return table.concat(lsp_names, ', ')
+end
+
+
 return {
   'nvim-lualine/lualine.nvim',
   opts = {
     options = {
       icons_enabled = false,
       theme = 'auto',
-      component_separators = '🐱',
-      section_separators = '',
     },
     sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch' },
       lualine_c = {
-        {
-          'filename',
-          path = 1,
-        }
-      }
+        { 'filename', path = 1 },
+      },
+      lualine_x = {
+        { lsp_status, icons_enabled = true, icon = ' LSP:' },
+      },
+      lualine_y = {
+        'encoding',
+        'fileformat',
+        'filetype'
+      },
     }
   },
 }
-
